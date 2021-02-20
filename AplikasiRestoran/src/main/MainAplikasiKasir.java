2c0b7c841b90251a67b39b76e814e482debba856
naufalakmal <muhammadnaufalakmal1@gmail.com>
2021-02-20 14:53:59 +0700

    Add Fitur Menambah dan Menampilkan Daftar Menu

--- AplikasiRestoran/src/main/MainAplikasiKasir.java
+++ AplikasiRestoran/src/main/MainAplikasiKasir.java
@@ -5,17 +5,53 @@
  */
 package main;
 
+import java.util.Scanner;
+
+import classes.DaftarMenu;
+import classes.Kuah;
+import classes.Minuman;
+import classes.Ramen;
+import classes.Toping;
+
+
 /**
  *
  * @author Akmal
  */
 public class MainAplikasiKasir {
+  
+    public DaftarMenu daftarMenu;
     
     public static void main(String[] args) {
+        // Inisialisasi kelas Scanner untuk mengambil input dari keyboard
+        Scanner input = new Scanner(System.in);
         
+        MainAplikasiKasir app = new MainAplikasiKasir();
+        // tampilkan daftar Menu
+        app.generateDaftarMenu();
     }
     
     public void generateDaftarMenu() {
+        daftarMenu = new DaftarMenu();
+        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
+        daftarMenu.tambahMenu(new Kuah("Kuah Orisinil"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Tomyam Spicy"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Rasa Indomie"));
+        daftarMenu.tambahMenu(new Toping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Toping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Toping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Toping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Toping("Enoki Goreng", 5000));
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SFC", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
         
+        daftarMenu.tampilDaftarMenu();
+
     }
 }
