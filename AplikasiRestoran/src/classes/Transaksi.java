fb4301a1fed3aab7a2feab511f20ec0c2a24a0b3
naufalakmal <muhammadnaufalakmal1@gmail.com>
2021-03-07 13:55:57 +0700

    Modul D: Mencetak Struk

--- AplikasiRestoran/src/classes/Transaksi.java
+++ AplikasiRestoran/src/classes/Transaksi.java
@@ -13,26 +13,34 @@ import java.util.ArrayList;
  */
 public class Transaksi {
     private String noTransaksi;
-    private String namaPemesanan;
+    private String namaPemesan;
     private String tanggal;
     private String noMeja;
     private ArrayList<Pesanan> pesanan;
     private double uangBayar;
     private double pajak;
     private double totalBayar;
+    // tambah
+    private double biayaService = 0;
 
     public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja) {
+        this.noTransaksi = no_transaksi;
+        this.namaPemesan = nm_pemesan;
+        this.tanggal = tanggal;
+        this.noMeja = no_meja;
+        
+        pesanan = new ArrayList<>();
         
     }
     
     public void tambahPesanan(Pesanan pesanan) {
-        
+        this.pesanan.add(pesanan);
     }
-    
-    public Pesanan getPesanan() {
-
-        return null;
-    }
+  
+    // hapus getPesanan dari struktur keals
+//    public Pesanan getPesanan() {
+//        return null;
+//    }
     
     public ArrayList<Pesanan> getSemuaPesanan() {
         
@@ -49,7 +57,67 @@ public class Transaksi {
         return 0;
     }
     
-    public void cetakStruk() {
+    public void cetakStruk () {
+        System.out.println("\n======== Oishi Ramen ========");
+        System.out.println("No Transaksi : " + noTransaksi);
+        System.out.println("Pemesan : " + this.namaPemesan);
+        System.out.println("Tanggal : " + this.tanggal);
         
+        // Cek jika nomor meja kosong, berarti take away
+        if(this.noMeja.equals("")){
+            this.noMeja = "Take Away";
+        }
+        
+        System.out.println("Meja : " + this.noMeja);
+        System.out.println("============================");
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan psn = pesanan.get(i);
+            Menu m = psn.getMenu();
+            String pesananStruk = psn.getJumlah() + " " + m.getNama_menu() + "\t" + (m.getHarga() * psn.getJumlah());
+            
+            // Jika pesanan kuah, tambah spasi di awal 2
+            if(m.getKategori().equals("Kuah")){
+                pesananStruk = "  " + pesananStruk;
+            }
+            
+            // Tampilkan pesanan
+            System.out.println(pesananStruk);
+        }
+    }
+    
+    // Tambahkan
+    public void setBiayaService(double biayaService){
+        this.biayaService = biayaService;
+    }
+    
+    public void setPajak(double pajak){
+        this.pajak = pajak;
+    }
+    
+    public double hitungTotalPesanan(){
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan psn = pesanan.get(i);
+            double harga = psn.getMenu().getHarga();
+            totalBayar += (harga * psn.getJumlah());
+        }
+        
+        return totalBayar;
+    }
+    
+    public double hitungPajak(){
+        return totalBayar * this.pajak;
+    }
+    
+    public double hitungBiayaService(){
+        return totalBayar * this.biayaService;
+    }
+    
+    public double hitungTotalBayar(double pajak, double biayaService){
+        totalBayar = totalBayar + pajak + biayaService;
+        return totalBayar;
+    }
+    
+    public double hitungKembalian (double uangBayar) {
+        return uangBayar - totalBayar;
     }
 }
